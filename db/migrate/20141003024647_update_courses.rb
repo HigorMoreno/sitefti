188b11a458f191ef03158969f70ef2a1211f932f
Higor Moreno Ribeiro <higormorenoribeiro@gmail.com>
2014-10-03 01:02:42 -0300

    Cleaning migrations

--- db/migrate/20141003024647_update_courses.rb
+++
@@ -1,8 +0,0 @@
-class UpdateCourses < ActiveRecord::Migration
-  def change
-  	change_table :courses do |t|
-      t.rename :description, :short_description
-      t.string :full_description, :limit => 1024
-    end
-  end
-end
